9e08e319b0f8859594edeb05a19f5a663727659d
Danil <lilkit1996@gmail.com>
2019-09-15 11:59:44 +0300

    Initial commit

---
+++ src/Lesson10.java
@@ -0,0 +1,48 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Lesson10 {
+
+    public String formatDate() {
+        String firstDate = "15.08.1995 18:30:15";
+        DateFormat df = new SimpleDateFormat("dd.MM.yyyy kk:mm:ss", Locale.US);
+        DateFormat destinationFormat = new SimpleDateFormat("dd MMM h:mm a",
+                Locale.US);
+        Date date;
+
+        String newDate;
+
+        try {
+            date = df.parse(firstDate);
+            newDate = destinationFormat.format(date);
+            System.out.println(newDate);
+
+            return newDate;
+        } catch (ParseException e) {
+            System.out.println("Can't parse");
+        }
+        return "";
+    }
+
+    public void writtenFile(String newDate) {
+
+        try {
+            FileWriter writer = new FileWriter("C:\\Agr\\lesson.txt");
+            writer.write(newDate);
+
+            writer.flush(); // как я понял метод flush() позволит производить перезапись файла дальше в программе
+                            // а если написать метод close() то перезаписывать файл будет нельзя
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Lesson10 lesson10 = new Lesson10();
+        lesson10.writtenFile(lesson10.formatDate());
+    }
+}
