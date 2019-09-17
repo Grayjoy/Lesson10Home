e1f21a1487529b0a1e1e62cc7de1ef17d0580920
Danil <lilkit1996@gmail.com>
2019-09-17 18:43:19 +0300

    1.04

--- src/Lesson10.java
+++ src/Lesson10.java
@@ -24,8 +24,6 @@ public class Lesson10 {
             newDate = destinationFormat.format(date);
             System.out.println(newDate);
 
-
-            return newDate;
         } catch (ParseException e) {
             System.out.println("Can't parse");
         }
